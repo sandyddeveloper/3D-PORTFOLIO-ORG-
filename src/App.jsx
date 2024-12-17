8e17b7f9b0180a0254ed2fb2acc1dbe4d59a6d89
DEVxNET-ORG <devxnetoffl@gmail.com>
2024-12-17 14:12:27 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { BrowserRouter } from "react-router-dom";
 
-import './App.css'
-function App() {
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import HireSection from "./components/HireSection";
+import Footer from "./components/Footer";
+
+const App = () => {
   return (
-    <div>this is main</div>
-  )
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <HireSection />
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HireSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HireSection = () => {
+  return (
+    <div>HireSection</div>
+  )
+}
+
+export default HireSection
