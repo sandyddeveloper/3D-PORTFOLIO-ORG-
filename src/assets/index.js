f605a91866cb2e7d68f58e455a82932b63f28019
DEVxNET-ORG <devxnetoffl@gmail.com>
2024-12-17 14:48:40 +0530

    init

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -13,22 +13,26 @@ import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
+import next from "./tech/nextjs.png";
+import dj from "./tech/dj.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import mysql from "./tech/mysql.png";
+import py from "./tech/py.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import webser from "./company/Web.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+
+import p1 from "./p1.png";
+import p2 from "./p2.png";
+import p3 from "./p3.png";
+import p4 from "./p4.png";
+import p5 from "./p5.png";
+import p6 from "./p6.png";
+import p7 from "./p7.png";
 
 export {
   logo,
@@ -45,18 +49,21 @@ export {
   git,
   html,
   javascript,
-  mongodb,
-  nodejs,
+  next,
+  dj,
   reactjs,
-  redux,
   tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  mysql,
+  py,
+  webser,
   carrent,
   jobit,
   tripguide,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
 };
