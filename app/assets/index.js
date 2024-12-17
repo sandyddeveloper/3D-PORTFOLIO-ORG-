7ebdf9e4c6dcd07b2dd80a106948857310a58c7b
DEVxNET-ORG <devxnetoffl@gmail.com>
2024-12-17 12:14:14 +0530

    navbar over

---
+++ app/assets/index.js
@@ -0,0 +1,69 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import next from "./tech/nextjs.png";
+import dj from "./tech/dj.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import mysql from "./tech/mysql.png";
+import py from "./tech/py.png";
+
+import webser from "./company/Web.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import p1 from "./p1.png";
+import p2 from "./p2.png";
+import p3 from "./p3.png";
+import p4 from "./p4.png";
+import p5 from "./p5.png";
+import p6 from "./p6.png";
+import p7 from "./p7.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  next,
+  dj,
+  reactjs,
+  tailwind,
+  mysql,
+  py,
+  webser,
+  carrent,
+  jobit,
+  tripguide,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+};
